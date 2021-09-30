1801ffd2e28118f5e5dd50756456b5415c3737ef
Vandana <aroravandana234@gmail.com>
2021-09-30 23:42:10 +0530

    leetcode solutions

---
+++ Binary-Search.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ans = -1;
+        int low = 0, high = nums.size()-1;
+        
+        
+        while(low<=high)
+        {
+              int mid = low + (high - low)/2;
+            
+            if(nums[mid] == target) return mid;
+            
+            else if(nums[mid]<target)
+            {
+                low = mid+1;
+            }
+            
+            else
+            {
+                high = mid -1;
+            }
+        }
+        return -1;
+      
+            
+    }
+};
