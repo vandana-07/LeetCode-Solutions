827253d6fb15770b220f8ca28f82493da6b52175
Vandana <aroravandana234@gmail.com>
2021-10-02 21:08:10 +0530

    leetcode questions

---
+++ Plus-One.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& dig) {
+        
+      int n=dig.size(),car=1;
+        
+        for(int i=n-1;i>=0;--i){
+            
+            int x = dig[i]+car;
+            dig[i]=x%10;
+            car = x/10;
+            if(car==0) break;
+        }
+        if(car>0) 
+            dig.insert(dig.begin(), car);
+        return dig;
+        
+    }
+};
