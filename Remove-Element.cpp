169fefacd152684e7f054e810065302872cbc85b
Vandana <aroravandana234@gmail.com>
2021-10-15 18:50:56 +0530

    leetcode solutions

---
+++ Remove-Element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        
+      auto it =remove(nums.begin(),nums.end(),val);
+        
+        return distance(nums.begin(),it);
+        
+    }
+};
