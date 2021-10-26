bade7a92b32140e9f3db8731567b52148e80ef51
Vandana <aroravandana234@gmail.com>
2021-10-27 01:54:42 +0530

    leetcode solutions

--- Intersection-of-two-arrays.cpp
+++ Intersection-of-two-arrays.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> intersect(vector<int>& nm1, vector<int>& nm2) {
         
-        int si = nums1.size();
+        int si = nm.size();
         vector<int>ans;
         map<int,int>m;
         
         for(int i =0;i<si;i++)
         {
-            m[nums1[i]]++;
+            m[nm1[i]]++;
         }
         
-        for(int i=0;i<nums2.size();i++)
+        for(int i=0;i<nm2.size();i++)
         {
-            if(m[nums2[i]]>0)
+            if(m[nm2[i]]>0)
                 
             {
-                ans.push_back(nums2[i]);
-                m[nums2[i]]--;
+                ans.push_back(nm2[i]);
+                m[nm2[i]]--;
             }
                 
         }
