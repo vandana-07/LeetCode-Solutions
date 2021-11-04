b1b9514f0cf6e91e742474780eac1a072bdbe912
Vandana <aroravandana234@gmail.com>
2021-11-04 23:54:34 +0530

    leetcode question

--- Palindrome-Number.cpp
+++ Palindrome-Number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
+    bool isPalindrome(int y) {
         
-      string ans = to_string(x);
+      string ans = to_string(y);
         reverse(ans.begin(), ans.end());
-        return to_string(x) == ans;
+        return to_string(y) == ans;
         
         
     }
